8db314fa50eea9d2f7a21753b5cad3ce5e9ac135
Anthony James <karl.a.james@hotmail.com>
2013-04-02 17:30:20 -0500

    Interfaced the data and server comm layers.

---
+++ TicTacTotalDomination.Util/DataServices/IGameDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TicTacTotalDomination.Util.Models;
+
+namespace TicTacTotalDomination.Util.DataServices
+{
+    public interface IGameDataService : IDisposable
+    {
+        Player GetOrCreatePlayer(string playerName);
+        Game CreateGame(Player playerOne, Player playerTwo);
+        Game GetGame(int gameId);
+        IEnumerable<GameMove> GetGameMoves(int gameId);
+        void Move(int gameId, int playerId, int? origX, int? origY, int x, int y);
+    }
+}
---
+++ TicTacTotalDomination.Util/NetworkCommunication/ICommunicationChannel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TicTacTotalDomination.Util.NetworkCommunication
+{
+    public enum StatusFlag{None, WinningMove, DrawMove, ChallengeWin, ChallengeMove, AcceptLoss}
+
+    [ServiceContract]
+    public interface ICommunicationChannel
+    {
+        ChallengeResult ChallengePlayer(string playerId, string opponentId);
+        MoveResult PostMove(int gameId, string playerId, int x, int y, StatusFlag flag);
+    }
+
+    [DataContract]
+    public class ChallengeResult
+    {
+        [DataMember(Name = "gameID")]
+        public int GameId { get; set; }
+        [DataMember(Name = "message")]
+        public string Messsage { get; set; }
+        [DataMember(Name = "challenger")]
+        public string OpponentId { get; set; }
+        [DataMember(Name = "yourTurn")]
+        public bool YourTurn { get; set; }
+        [DataMember(Name = "x")]
+        public int X { get; set; }
+        [DataMember(Name = "y")]
+        public int Y { get; set; }
+        [DataMember(Name = "error")]
+        public string Error { get; set; }
+    }
+
+    [DataContract]
+    public class MoveResult
+    {
+        [DataMember(Name = "x")]
+        public int X { get; set; }
+        [DataMember(Name = "y")]
+        public int Y { get; set; }
+        [DataMember(Name = "time")]
+        public DateTime MoveDatetime { get; set; }
+        [DataMember(Name = "flags")]
+        public string StatusFlag { get; set; }
+        [DataMember(Name = "error")]
+        public string Error { get; set; }
+    }
+}
