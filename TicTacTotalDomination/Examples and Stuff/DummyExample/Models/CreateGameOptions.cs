88f182b23e5c94095d1be3d8e825e489125dc9b9
Gary Michaud <okgrito@gmail.com>
2013-04-19 22:33:42 -0500

    -a

---
+++ TicTacTotalDomination/Examples and Stuff/DummyExample/Models/CreateGameOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DummyExample.Models
+{
+    public enum GameModes{Local, Network}
+
+    public class CreateGameOptions
+    {
+        //If you want properties of your models/view models to bind
+        //  , they need to be public properties like this.
+        //Fields like string PlayerName; wont work.
+        public string PlayerOneName { get; set; }
+        public string PlayerTwoName { get; set; }
+
+        //MVC model binding can bind just about anything.
+        //Including enum values.
+        public GameModes PlayerMode { get; set; }
+
+        public int NumberOfPlayers { get; set; }
+    }
+}
---
+++ TicTacTotalWorkingAI/TicTacTotal/Move.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacTotal
+{
+    class Move
+    {
+        public int x;
+        public int y;
+
+        public int getX()
+        {
+            return x;
+        }
+        public void setX(int xPos)
+        {
+            this.x = xPos;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public void setY(int yPos)
+        {
+            this.y = yPos;
+        }
+
+        public void set(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
